97483bc39e310c95ed2a1d849531a244bfd85688
nzwh <ivanisaiahserato@gmail.com>
2024-03-23 10:55:37 +0800

    Add polished functions
    Now a working prototype, to be throughly tested before shipment.

---
+++ logic.ts
@@ -0,0 +1,89 @@
+// offsets the decimal point given an offset
+function offset(number: number, offset: number) {
+  number = Math.abs(number);
+  let intg = number.toString().split('.')[0] || '0';
+  let frac = number.toString().split('.')[1] || '';
+
+  return (offset < 0) ? 
+  [intg.slice(0, offset) == '' ? '0' : intg.slice(0, offset), "0".repeat(Math.max(0, Math.abs(offset) - intg.length)) + intg.slice(offset) + frac].join('.') :
+  [intg + frac.slice(0, offset) + "0".repeat(Math.max(0, Math.abs(offset) - frac.length)), frac.slice(offset)].join('.');
+}
+
+// displaces a number by a given exponent (turns exponent to 0)
+function displace10(mantissa: number, exponent: number): string {
+  let result = (exponent == 0) ? Math.abs(mantissa) : offset(mantissa, exponent);
+  return (mantissa < 0) ? "-" + result : "" + result;
+}
+
+// converts a number to its binary equivalent
+function binary(number: number) {
+  return number.toString(2);
+}
+
+// displace the decimal point to follow ^-1
+function normalize(number: string): [string, number] {
+  if (number[0] == '-') number = number.substring(1);
+  let position = (number.indexOf(".") == -1) ? 
+    number.length : number.indexOf(".");
+  let intg = number.toString().split('.')[0] || '0';
+  let frac = number.toString().split('.')[1] || '';
+
+  let exponent = parseFloat(intg) != 0 ? 
+    intg.indexOf("1") + 1 - position : frac.indexOf("1") + 1;
+
+  let result = number.replace(".", "");
+  return [result.substring(0, position + exponent).replace(/^0+/, '') +   
+    "." + result.substring(position + exponent), -exponent];
+}
+
+function get_results(n: string, b: number, e: number): string[] {
+  let negative = (n[0] == '-') ? "1" : "0";
+  let number = parseFloat(n);
+
+  let displaced = displace10(number, e);
+  let displaced_binary = (b == 10) ? binary(parseFloat(displaced)) : displaced.toString();
+  let [normalized, exponent] = normalize(displaced_binary);
+
+  let sign = negative;
+  let exponent_bits = "", mantissa = "";
+  let s_case = "";
+
+  console.log(exponent);
+
+  if (number == 0) {
+    s_case = "Zero";
+    exponent_bits = "0".repeat(11);
+    mantissa = "0".repeat(52);
+
+  } else if (isNaN(number)) {
+    s_case = "NaN";
+    exponent_bits = "1".repeat(11);
+    mantissa = "1".repeat(52);
+
+  } else if (exponent + 1023 < 0) {
+    s_case = "Denormalized";
+    exponent_bits = "0".repeat(11);
+    mantissa = offset(number, exponent + 1023 - 1).split(".")[1].padEnd(52, "0");
+
+  } else if (e > 1023) {
+    s_case = "Infinity";
+    exponent_bits = "1".repeat(11);
+    mantissa = "0".repeat(52);
+
+  } else {
+    s_case = "";
+    exponent_bits = (exponent + 1023).toString(2).padStart(11, "0");
+    mantissa = (!isNaN(parseFloat(normalized)) ? 
+      normalized.substring(Math.max(normalized.indexOf(".") + 1, 1)) : "").padEnd(52, "0");
+  }
+
+  let hex = '0x' + (
+  parseInt(sign + exponent_bits, 2).toString(16).toUpperCase() +
+  parseInt(mantissa, 2).toString(16).toUpperCase().padEnd(13, "0")).padStart(16, "0");
+
+  let n_binary = `${normalized} x ${2}^${exponent}`;
+
+  return [displaced_binary, n_binary, sign, exponent_bits, mantissa, hex, s_case];
+}
+
+export { get_results };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,45 @@
 "use client"
 
 import React, { useState } from "react";
-import Image from "next/image";
+// import logic ts file
+import { get_results } from "../../logic";
 
 export default function Home() {
+
   const [baseValue, setBaseValue] = useState(2); 
   const [numberValue, setNumberValue] = useState("");
   const [exponentValue, setExponentValue] = useState("");
+
   const [input, setInput] = useState<{ number: string; base: number; exponent: number; } | null>(null);
+  const [output, setOutput] = useState<{ displaced_binary: string; n_binary: string; sign: string; exponent_bits: string; mantissa: string; hex: string; s_case: string; full_output: string} | null>(null);
+
   const [showOutput, setShowOutput] = useState(false);
+  const [groups, setGroups] = useState<string[] | null>(null);
+
+  const handleReset = (event: any) => {
+    setNumberValue("");
+    setExponentValue("");
+    setInput(null);
+    setOutput(null);
+    setShowOutput(false);
+  };
 
   const handleBaseChange = (event: any) => {
-    setNumberValue(""); // Clear number field
-    setExponentValue(""); // Clear exponent field
+    if (showOutput) setShowOutput(false);
+    setNumberValue("");
+    setExponentValue("");
     setBaseValue(parseInt(event.target.value));
   };
 
-  const handleExponentChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
-    const newValue = e.target.value.slice(0, 5); // Limits the input to 5 characters
-    setExponentValue(newValue);
+  const handleExponentChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    if (showOutput) setShowOutput(false);
+    setExponentValue(event.target.value.slice(0, 5));
   };
 
   const handleNumberChange = (event: any) => {
+    if (showOutput) setShowOutput(false);
+    if (baseValue === 2)
+      if (!/^[01.-]*$/.test(event.target.value)) return;
     setNumberValue(event.target.value);
   };
 
@@ -34,37 +52,97 @@ export default function Home() {
     
     setInput(inputData); 
     setShowOutput(true);
+
+    let [displaced_binary, n_binary, sign, exponent_bits, mantissa, hex, s_case] = get_results(inputData.number, inputData.base, inputData.exponent);
+    let full_output: string = sign + exponent_bits + mantissa;
+
+    setOutput({ displaced_binary, n_binary, sign, exponent_bits, mantissa, hex, s_case, full_output });
+    setGroups(full_output.match(/.{1,4}/g));
+
+    console.log(s_case);
   };
 
   return (
-    <main className="h-screen w-screen bg-slate-600 flex items-center justify-center">
-      <section id="container" className=" w-[80rem] h-[40rem] bg-white text-black">
+    <main className="h-screen w-screen bg-[#D8DFE5] flex items-center justify-center">
+      <section id="container" className="w-[80rem] h-[40rem] bg-gradient-to-b from-[#F0F6F9] to-[#e2e9ed] text-black rounded shadow-2xl shadow-slate-300 flex flex-col items-center justify-center transition-transform px-8 gap-8">
 
-        {/* radio button for binary and decimal */}
-        <div className="flex space-x-4 text-black">
-          <label htmlFor="binary">Binary</label>
-          <input type="radio" name="conversion" id="binary" value="2" onChange={handleBaseChange} defaultChecked />
-          <label htmlFor="decimal">Decimal</label>
-          <input type="radio" name="conversion" id="decimal" value="10" onChange={handleBaseChange} />
-        </div>
-      
-          {/* input field */} 
-          <label htmlFor="input">Enter a number</label>
-        <input type="number" id="input" step="any" className="w-[20rem] h-[3rem] bg-gray-200 text-black" value={numberValue} onChange={handleNumberChange} />
+        <form onSubmit={handleCalculate} action="javascript:void(0);" className="w-auto h-auto flex flex-col gap-4 items-center">
 
-        {/* exponent */}
-        <label htmlFor="exponent">Enter the exponent</label>
-        <input type="number" id="exponent" step="any" className="w-[20rem] h-[3rem] bg-gray-200 text-black" value={exponentValue} onChange={handleExponentChange} />
+          <div className="flex space-x-4 text-zinc-600">
+            <p className="mr-4">Select base number: </p>
+            <label htmlFor="binary">Binary (base-2)</label>
+            <input type="radio" name="conversion" id="binary" value="2" onChange={handleBaseChange} defaultChecked />
+            <label htmlFor="decimal">Decimal (base-10)</label>
+            <input type="radio" name="conversion" id="decimal" value="10" onChange={handleBaseChange} />
+          </div>
+          
+          <section className="flex items-center justify-center space-x-4">
+            <input type="number" id="input" step="any" className="bg-white text-zinc-800 outline-none text-5xl px-5 py-2 w-[56rem] h-20 rounded hover:bg-slate-200 transition colors duration-100 shadow-2xl shadow-zinc-200 border-zinc-100 border-2" placeholder={"1.00"} value={numberValue} onChange={handleNumberChange} required/>
+            <h1 className="text-4xl select-none">*</h1>
 
-        {/* button */}
-        <button onClick={handleCalculate}  className="w-[10rem] h-[3rem] bg-black text-white">Calculate</button>
+            <div className="flex items-start gap-2">
+              <div className="bg-white text-zinc-800 outline-none text-5xl h-20 px-5 py-2 flex items-center rounded hover:bg-slate-200 transition colors duration-100 shadow-2xl shadow-zinc-200 border-zinc-100 border-2 select-none">
+                <h1>{baseValue}</h1>
+              </div>
+
+              <input type="number" id="exponent" step="any" className="w-[6rem] outline-none bg-white text-zinc-800 text-2xl px-2 py-1 rounded hover:bg-slate-200 transition colors duration-100 shadow-2xl shadow-zinc-200 border-zinc-100 border-2" placeholder={"0"} value={exponentValue} onChange={handleExponentChange} required/>
+            </div>
+          </section>
+
+          {/* special case */}
+          {output?.s_case != "" && showOutput && (
+            <div className="flex flex-col gap-4 text-red-600 font-medium">
+              <h1>Special case: {output?.s_case}</h1>
+            </div>
+          )}
+
+          {/* buttons */}
+          <div className="flex flex-row gap-4">
+            <button className="w-[8rem] h-[2.5rem] text-white rounded bg-gradient-to-b from-violet-300 to-violet-400 shadow-xl hover:shadow-sm transition-shadow duration-200">Calculate</button>
+            <button onClick={handleReset} className="w-[8rem] h-[2.5rem] text-white rounded bg-gradient-to-b from-violet-300 to-violet-400 shadow-xl hover:shadow-sm transition-shadow duration-200">Reset</button>
+          </div>
+        </form>
+
 
         {/* output */}
         {showOutput && (
-          <div className="mt-4">
-            <p>Base: {input?.base}</p>
+          <div className="flex flex-col gap-8">
+            
+            <div className="flex flex-col items-center gap-3">
+            <h1 className="text-zinc-600">Raw binary representation</h1>
+            <div className="flex flex-row flex-wrap gap-4 items-center justify-center">
+              {groups?.map((group, index) => (
+                <div key={index} className="w-auto h-8 text-2xl flex items-center gap-2">{group.split("").map((value, index) => {
+                  return <span key={index} className="text-2xl bg-white px-2 py-1 shadow-xl shadow-zinc-200 rounded hover:bg-slate-200 transition colors duration-200 select-none">{value}</span>
+                })}</div>
+              ))}
+            </div>
+            </div>
+            
+            <div className="flex flex-col items-center gap-3">
+            <h1 className="text-zinc-600">Hex representation</h1>
+            <div className="w-[50rem] h-8 text-2xl flex items-center justify-center gap-2">
+              <span className="text-2xl bg-white px-2 py-1 shadow-xl shadow-zinc-200 rounded hover:bg-slate-200 transition colors duration-200 select-none">{"0x"}</span>
+              {output?.hex.substring(2).split("").map((value, index) => {
+                return <span key={index} className="text-2xl bg-white px-2 py-1 shadow-xl shadow-zinc-200 rounded hover:bg-slate-200 transition colors duration-200 select-none">{value}</span>
+              })}
+            </div>
+            </div>
+
+
+
+            {/* <p>Base: {input?.base}</p>
             <p>Number: {input?.number}</p>
             <p>Exponent: {input?.exponent}</p>
+
+            <p>Binary: {output?.displaced_binary}</p>
+            <p>Normalized: {output?.n_binary}</p>
+
+            <p>Sign: {output?.sign}</p>
+            <p>Exponent Bits: {output?.exponent_bits}</p>
+            <p>Mantissa: {output?.mantissa.match(/.{1,8}/g)?.join(" ")}</p>
+            <p>Hex: {output?.hex}</p>
+            <p>Case: {output?.s_case}</p> */}
           </div>
         )}
       </section>
